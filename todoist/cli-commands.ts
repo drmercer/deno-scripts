259c3955bcec91ca05bb3977e0be408a4915a296
Dan Mercer <danielmercer1@gmail.com>
2021-06-16 23:47:19 -0600

    Refactor CLI commands to be more reusable in other CLIs

---
+++ todoist/cli-commands.ts
@@ -0,0 +1,55 @@
+import password from "../io/password.ts";
+import { readState, writeState } from "./store.ts";
+import { doSync, SyncState } from "./sync.ts";
+
+export function init() {
+  const accessToken = password("Enter your Todoist API token: ");
+  if (accessToken === undefined) {
+    console.warn("Aborted. No access token entered.");
+
+  } else if (accessToken === '') {
+    localStorage.removeItem("todoist:token");
+    console.error("Access token cleared under origin " + window.location.origin);
+
+  } else {
+    localStorage.setItem("todoist:token", accessToken);
+    console.log("Access token saved under origin " + window.location.origin);
+  }
+}
+
+export async function sync() {
+  const accessToken = localStorage.getItem("todoist:token");
+  if (!accessToken) {
+    console.error("Run td init first to set your Todoist access token");
+    return;
+  }
+  const oldState = readState();
+
+  console.log("Syncing...");
+  const state = await doSync(accessToken, oldState);
+
+  writeState(state);
+
+  console.log("State saved:", summarizeState(state));
+}
+
+export function status() {
+  const state = readState();
+  if (state) {
+    console.log("Current Todoist state:", summarizeState(state));
+  } else {
+    console.log("No Todoist state found. Do a sync first.");
+  }
+}
+
+export function clear() {
+  writeState(undefined);
+}
+
+// A little helper for summarizing the currently synced state
+function summarizeState(state: SyncState) {
+  return {
+    tasks: state.tasks.length,
+    projects: state.projects.length,
+  };
+}
--- todoist/demo.ts
+++ todoist/demo.ts
@@ -1,60 +1,22 @@
 import { endOfDay } from "../date/relative.ts";
-import password from "../io/password.ts";
 import { compareCreatedDate, compareDueDate, comparePriority } from "./comparators.ts";
-import { readState, writeState } from "./store.ts";
-import { doSync, SyncState } from "./sync.ts";
+import { readState } from "./store.ts";
+import { SyncState } from "./sync.ts";
+
+import * as coreCommands from "./cli-commands.ts";
+
+const commands: Record<string, () => Promise<void>|void> = {
+  ...coreCommands,
+  inbox,
+}
 
 const command = Deno.args[0];
 
-if (command === "init") {
-  init();
-} else if (command === "sync") {
-  await sync();
-} else if (command === "summary") {
-  summary();
-} else if (command === "inbox") {
-  inbox();
-} else if (command === "clear") {
-  clear();
+if (command in commands) {
+  await commands[command]();
 } else {
-  console.error(`USAGE: demo <init|sync|summary|inbox|clear>`);
+  console.error(`USAGE: demo <${Object.keys(commands).join('|')}>`);
   Deno.exit(1);
-}
-
-function init() {
-  const accessToken = password("Enter your Todoist API token: ");
-  if (accessToken === undefined) {
-    console.warn("Aborted. No access token entered.");
-  } else if (accessToken === '') {
-    localStorage.removeItem("todoist:token");
-    console.error("Access token cleared under origin " + window.location.origin);
-  } else {
-    localStorage.setItem("todoist:token", accessToken);
-    console.log("Access token saved under origin " + window.location.origin);
-  }
-}
-
-async function sync() {
-  const accessToken = localStorage.getItem("todoist:token");
-  if (!accessToken) {
-    console.error("Run td init first to set your Todoist access token");
-    return;
-  }
-  const oldState = readState();
-  const state = await doSync(accessToken, oldState);
-
-  writeState(state);
-
-  console.log("Data saved.", summarizeState(state));
-}
-
-function summary() {
-  const state = readState();
-  if (state) {
-    console.log("Todoist state: ", summarizeState(state));
-  } else {
-    console.log("No Todoist state found. Do a sync first.");
-  }
 }
 
 function inbox() {
@@ -75,17 +37,4 @@ function inbox() {
     .map(t => t.content + (t.due ? ` (${t.due.date}, ${t.due.string})` : ""))
 
   console.log(tasks)
-}
-
-function clear() {
-  writeState(undefined);
-}
-
-// ====================
-
-function summarizeState(state: SyncState) {
-  return {
-    tasks: state.tasks.length,
-    projects: state.projects.length,
-  };
 }
