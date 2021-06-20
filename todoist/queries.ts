857d1378894a7f87161b9713799ce0562ecc8d24
Dan Mercer <danielmercer1@gmail.com>
2021-06-19 20:13:34 -0600

    Change dateStringToDate() to interpret date-only strings as at the end of the day

--- todoist/queries.ts
+++ todoist/queries.ts
@@ -8,17 +8,14 @@ export const dateStringHasDueTime = (s: string) => s.includes(":");
 export const hasDueTime = (t: Task) => !!t.due && dateStringHasDueTime(t.due.date);
 
 // Need to ensure due date includes a time so JS will interpret it in local time instead of UTC.
-const dateStringToDate = (s: string) => dateStringHasDueTime(s) ? new Date(s) : new Date(s + "T00:00:00")
+const dateStringToDate = (s: string) => dateStringHasDueTime(s) ? new Date(s) : new Date(s + "T23:59:59")
 
 export const getDueDate = (t: Task): Date | undefined => t.due ? dateStringToDate(t.due.date) : undefined;
 
 /**
- * Returns true if the due date is today (or earlier) and the due time is unset or earlier than the current time.
+ * Returns true if the due date is today (or earlier) and the due time is earlier than the current time.
  */
-export const isDueNow = (t: Task) => {
-  const date = getDueDate(t);
-  return date ? date < new Date() : false;
-}
+export const isDueNow = (t: Task) => (getDueDate(t) || Number.POSITIVE_INFINITY) < new Date();
 
 export const hasParentTask = (t: Task) => !!t.parent_id;
 
